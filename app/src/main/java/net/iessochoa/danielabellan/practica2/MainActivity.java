3f6d50eb8387d42da888844050da401341d618ad
DanielAZ <danielabezarate@gmail.com>
2021-09-30 19:02:43 +0200

    Creación de ActivityAgenda y actualización de MainActivity

--- app/src/main/java/net/iessochoa/danielabellan/practica2/MainActivity.java
+++ app/src/main/java/net/iessochoa/danielabellan/practica2/MainActivity.java
@@ -1,13 +1,30 @@
 package net.iessochoa.danielabellan.practica2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAgenda;
+    private Button btnAgendaConstraint;
+    private Button btnReceta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAgenda = findViewById(R.id.btnAgenda);
+        btnAgendaConstraint = findViewById(R.id.btnAgendaConstraint);
+        btnReceta = findViewById(R.id.btnReceta);
+
+        btnAgenda.setOnClickListener(view ->{
+            Intent intent = new Intent(MainActivity.this, AgendaActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
