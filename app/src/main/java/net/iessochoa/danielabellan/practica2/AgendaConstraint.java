c826a41ac71e44bcfff5f78d04c2be564da5ee1e
DanielAZ <danielabezarate@gmail.com>
2021-10-14 11:51:07 +0200

    Implementación visual de AgendaConstraint

---
+++ app/src/main/java/net/iessochoa/danielabellan/practica2/AgendaConstraint.java
@@ -0,0 +1,13 @@
+package net.iessochoa.danielabellan.practica2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendaConstraint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda_constraint);
+    }
+}
